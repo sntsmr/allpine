d270f2846f63027a723b060a4665bed7d467ef5d
cacel santos <sntmsr@gmail.com>
2012-03-08 23:26:27 -0700

    second testing

---
+++ aboutus-log-home.php
@@ -0,0 +1,130 @@
+﻿
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+<meta name="country" content="USA"/>
+<meta name="location" content="North America > USA > Colorado > Southern Colorado > La Jara"/>
+<meta name="language" content="en-us"/>
+<meta name="rating" content="Safe For Kids"/>
+<meta name="rating" content="general"/>
+
+<meta name="keywords" content="All About Us, Finest Colorado Log Homes, Allpine Lumber Co." />
+
+
+<meta name="description" content="Allpine Lumber supplies house logs and other materials for log homes. Our priority has always been, and will always be, quality and integrity in our products." />
+            
+<meta name='googlebot' content='index, follow' />
+<meta name="robots" content="index,follow"/>
+
+        <title>All About Us - Finest Colorado Log Homes | Allpine Lumber Co.</title>
+
+        
+        <link href="css/allpineloghomes.css" rel="stylesheet" type="text/css"/>
+        <script src="nav2/SpryMenuBar.js" type="text/javascript"></script>
+        <link href="nav2/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+        
+        
+        <!-- para menu2 -->
+        <script language="javascript" type="text/javascript" src="css/js/menu1.js"> </script>
+        
+        <link href="css/intern.css" rel="stylesheet" type="text/css"/>
+
+     
+    </head>
+    <body>
+        <div id="wrapper">
+
+            <!--/////////Inicio wrapper /////-->
+            <?php include 'include/header.php' ?>
+
+            <!--/////////Inicio content /////-->
+            <br class="clearfloat" />
+            
+            <div id="content">
+                
+                <div id="frameDescription">
+                   
+                    <div id="contentDescription">
+                       <div id="backgroundDescriptionTop"></div>
+                       
+                       <div id="backgroundDescription">
+                        <div id="solo">
+	                    <div id="titleFrame">
+                        <div id="title">
+                            <h3>About Allpine Lumber Co</h3>
+                        </div>
+                        <div id="moreTitle">
+                            
+                        </div>
+                    </div>
+
+                        <blockquote>
+                            <p align="justify">   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;In the early 1940's, Allpine Lumber began as a small lumber mill, focused principally on producing rough-cut dimension lumber for the residents of a 
+                                small part of southern Colorado. Today, Allpine Lumber is one of the principal suppliers of house logs and other planed materials for log homes, not only 
+                                for Colorado, but also for most of the Midwest and other parts of the United States.</p>
+
+                            <p> Our priority has always been, and will always be, quality and integrity both in our products and in our service.<br /> We keep up with the most modern 
+                                equipment and technology in order to provide the best products. Customer satisfaction is important to us. If you are not completely satisfied with your 
+                                purchase, let us know and we'll resolve it.</p>
+
+                            <h3>TESTIMONIALS</h3>
+                            <p>&quot;I built log homes for many years in the Midwest, using different logs from different companies. I came to the conclusion that shrinkage and untrue logs were just part of the business-- not true. I moved to Colorado and began building with Allpine’s logs. I’ve built 4 homes since then and can’t believe how square they are. I’ve experienced virtually no shrinkage and can truly say these are some of the best logs I’ve ever used.&quot;</p>
+
+                            <p>- Ollie Miller, Southern Colorado Contractor <br /> </p>
+                                <a href="http://www.omillerconstruction.com"><span lang="">www.omillerconstruction.com</span></a><br />
+                             <p>       
+                                "They (Allpine Lumber) have an intimate knowledge of the industry... from tree harvest, milling, drying, preparation and delivery. AND they had my coveted plus, <strong>experience in building log cabins</strong>. They were and are truly wonderful to work with…my wife and I just unwrapped a bunk of their logs tonight as we were building, and they just make you stop and look at them. You can’t help it, the logs are beautiful!"<br />
+                                <br />
+                                - Doug Devore, Cabin Rental Business Owner  <br />
+                                <a href="http://www.cabinstartup.com"><span lang="">www.cabinstartup.com</span></a>
+                            </p>
+                                
+                                </blockquote>
+                                
+                          </div> <!-- end solo -->
+                          
+                          </div> <!--end backgroundDescription--> 
+                          <div id="backgroundDescriptionBottom"></div>
+                          
+                        
+
+                       
+                    </div><!-- end contentDescription-->
+                </div>
+
+
+
+                
+             
+                
+                
+
+                <div id="sitemap">
+                    <?php include 'include/sitemap.php' ?>
+                </div>
+                <!--/////////Fin sitemap /////-->
+            </div>
+            <!--/////////Fin content /////-->
+            <div class="clearfloat"/>
+            
+            <!--/////////call function footer /////-->
+            <?php include 'include/footer.php'; ?>
+        </div>
+        <!--/////////Fin wrapper /////-->
+        
+        <!-- Begin OrangeSoda Tracking Code -->
+        <script language="javascript" type="text/javascript"> 
+            var trackCid = 126518; 
+            var trackTid = '';
+        </script> 
+        <script type="text/javascript"> 
+            var proto = (("https:" == document.location.protocol) ? "https://" : "http://"); 
+            document.write(unescape("%3Cscript src='" + proto + "otracking.com/js/TrackingV2.js' type='text/javascript'%3E%3C/script%3E")); 
+        </script> 
+        <!-- End OrangeSoda Tracking Code -->
+
+
+    </body>
+</html>
